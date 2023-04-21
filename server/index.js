8aebb6d2941d3d85ea4eb26dd588f02c26263e6f
moviefan322 <philman202@yahoo.com>
2023-04-21 00:36:51 -0400

    installs path

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
