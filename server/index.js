ba3664f17d8d86f82644c0048a98aef38c080cd0
moviefan322 <philman202@yahoo.com>
2023-04-21 00:26:37 -0400

    adds get to server pointing to index.html

--- server/index.js
+++ server/index.js
@@ -22,4 +22,9 @@ app.use(
   })
 );
 
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
 app.listen(port, console.log(`Server started on port ${port}`));
