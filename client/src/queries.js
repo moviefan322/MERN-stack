cdd74c2e82c9a8105ff52dcb07725ca75b0ca2cd
moviefan322 <philman202@yahoo.com>
2023-04-20 20:29:53 -0400

    server functioning, graphql responding to query

---
+++ client/src/queries.js
@@ -0,0 +1,17 @@
+import { gql } from "@apollo/client";
+
+const GET_RESPONSES = gql`
+  query getResponses {
+    responses {
+        id
+        name
+        yesno
+        day
+        location
+        venue
+      }
+    }
+  }
+`;
+
+export { GET_RESPONSES };
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose");
-const { Response } = require("../models/Response");
+const Response = require("../models/Response.js");
 const responseSeeds = require("./responseSeeds.json");
-
+require("dotenv").config();
 console.log("MONGO URI:", process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 
